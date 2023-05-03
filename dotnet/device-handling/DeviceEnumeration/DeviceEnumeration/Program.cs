b35fc1258d02754a3fb86f28995be6dfb44e1ad3
Tim Cassens <tim.cassens@theimagingsource.com>
2023-05-03 16:09:06 +0200

    Add dotnet DeviceEnumeration example

---
+++ dotnet/device-handling/DeviceEnumeration/DeviceEnumeration/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace DeviceEnumeration
+{
+    internal class Program
+    {
+        static string FormatDeviceInfo(ic4.DeviceInfo deviceInfo)
+        {
+            return $"Model: {deviceInfo.ModelName} Serial: {deviceInfo.Serial} Version {deviceInfo.Version}";
+        }
+
+        static void PrintDeviceList()
+        {
+            Console.WriteLine("Enumerating all attached video capture devices in a single list...");
+
+            var deviceList = ic4.DeviceEnum.Devices.ToList();
+
+            if (deviceList.Count == 0)
+            {
+                Console.WriteLine("No devices found");
+            }
+
+            Console.WriteLine($"Found {deviceList.Count} devices:");
+
+            foreach( var deviceInfo in deviceList )
+            {
+                Console.WriteLine($"\t{FormatDeviceInfo(deviceInfo)}");
+            }
+
+            Console.WriteLine();
+        }
+
+        static void PrintInterfaceDeviceTree()
+        {
+            Console.WriteLine("Enumerating video capture devices by interface...");
+
+            var interfaceList = ic4.DeviceEnum.Interfaces.ToList();
+
+            foreach (var itf in interfaceList)
+            {
+                Console.WriteLine($"Interface: {itf.Name}");
+                Console.WriteLine($"\tProvided by {itf.TransportLayerName} [TLType: {itf.TransportLayerType}]");
+
+                var deviceList = itf.Devices.ToList();
+
+                if (deviceList.Count == 0)
+                {
+                    Console.WriteLine("\tNo devices found");
+                }
+
+                Console.WriteLine($"\tFound {deviceList.Count} devices:");
+
+                foreach (var deviceInfo in deviceList)
+                {
+                    Console.WriteLine($"\t\t{FormatDeviceInfo(deviceInfo)}");
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            PrintDeviceList();
+            PrintInterfaceDeviceTree();
+        }
+    }
+}
