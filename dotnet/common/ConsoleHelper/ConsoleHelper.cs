10ebfaee17e88a0cc3380d1ea2132b50b9131eac
Tim Cassens <tim.cassens@theimagingsource.com>
2023-07-25 16:36:41 +0200

    Add EventLine1Edge example program

---
+++ dotnet/common/ConsoleHelper/ConsoleHelper.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ic4
+{
+    public static class ConsoleHelper
+    {
+        public static T PresentUserChoice<T>(IReadOnlyList<T> items, Func<T, string> getName, string header) where T : class
+        {
+            Console.WriteLine(header);
+
+            for (int index = 0; index < items.Count; ++index)
+            {
+                var itemName = getName(items[index]);
+
+                Console.WriteLine($"[{index}] {itemName}");
+            }
+
+            while (true)
+            {
+                Console.Write("Select index: ");
+
+                var input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("> Cancelled");
+                    return null;
+                }
+
+                if (!int.TryParse(input, out int selectedIndex))
+                {
+                    Console.WriteLine("> Invalid input");
+                    continue;
+                }
+
+                if (selectedIndex < 0 || selectedIndex >= items.Count)
+                {
+                    Console.WriteLine("> Invalid index");
+                    continue;
+                }
+
+                return items[selectedIndex];
+            }
+        }
+
+        public static DeviceInfo PresentUserChoice(IReadOnlyList<DeviceInfo> deviceList, string prompt)
+        {
+            Func<DeviceInfo, string> buildDeviceString = (DeviceInfo di) =>
+            {
+                return $"{di.ModelName} ({di.Serial}) [{di.Interface.TransportLayerName}]";
+            };
+
+            return PresentUserChoice(deviceList, buildDeviceString, prompt);
+        }
+    }
+}
