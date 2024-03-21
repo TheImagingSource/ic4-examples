4545539a1761c622bc72718bdaf17fe472ee6f82
Kevin Herbrich <kevin.herbrich@theimagingsource.com>
2024-03-21 08:26:17 +0000

    Added new dotnet examples

---
+++ dotnet/device-handling/DeviceListChanged/DeviceListChanged/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DeviceListChanged
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ic4.Library.Init();
+
+            var deviceEnum = new ic4.DeviceEnum();
+            deviceEnum.DeviceListChanged += DeviceListChanged;
+
+            var deviceCount = ic4.DeviceEnum.Devices.Count;
+
+            Console.WriteLine("Press ENTER to exit program");
+            Console.WriteLine($"{deviceCount} devices connected initially.");
+            Console.WriteLine("");
+
+            Console.ReadKey();
+
+            deviceEnum.Dispose();
+
+            return;
+        }
+
+        static void DeviceListChanged(object sender, EventArgs e)
+        {
+            var deviceList = ic4.DeviceEnum.Devices;
+
+            var newDeviceCount = deviceList.Count;
+
+            Console.WriteLine("Device list has changed!");
+            Console.WriteLine($"Found {newDeviceCount} devices");
+            Console.WriteLine("");
+        }
+    }
+}
