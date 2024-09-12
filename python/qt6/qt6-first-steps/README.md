353bfe1a0beb71e053569c0406efa84af1ec4dcc
Tim Cassens <tim.cassens@theimagingsource.com>
2024-09-12 10:44:50 +0200

    Rework qt6-first-steps to use ic4.pyside6

---
+++ python/qt6/qt6-first-steps/README.md
@@ -0,0 +1,12 @@
+Qt6 First Steps with Python
+===========================
+
+This example shows the steps necessary to display live images from a camera in a PySide6 (Qt6) application.
+
+To run, the following packages must be installed in the active Python environment:
+
+- PySide6
+- imagingcontrol4
+- imagingcontrol4pyside6
+
+The example makes use of the pre-built device selection dialog (`imagingcontrol4.pyside6.DeviceSelectionDialog`) and the display widget (`imagingcontrol4.pyside6.DisplayWidget`).
--- python/qt6/qt6-first-steps/qt6-first-steps.py
+++ python/qt6/qt6-first-steps/qt6-first-steps.py
@@ -1,11 +1,6 @@
-from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
+from PySide6.QtWidgets import QApplication, QMainWindow, QDialog
 
 import imagingcontrol4 as ic4
-
-
-def _window_handle(wnd: QWidget) -> int:
-    # Helper function to get window handle from a QWidget
-    return wnd.winId().__int__()
 
 
 class MainWindow(QMainWindow):
@@ -16,24 +11,25 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("IC Imaging Control 4 Python Library - First Steps")
         self.resize(1024, 768)
 
-        # Create a widget to use as the target for video display
-        video_widget = QLabel("No device selected")
-        self.setCentralWidget(video_widget)
-
         # Create a Grabber object to communicate with a video capture device
         self.grabber = ic4.Grabber()
 
-        # Show the builtin dialog to let the user select a device
+        # Show the pre-built dialog to let the user select a device
         # If the user selects a device, the function opens it in the grabber object
-        if not ic4.Dialogs.grabber_select_device(self.grabber, _window_handle(self)):
+        dlg = ic4.pyside6.DeviceSelectionDialog(self.grabber, self)
+        if not dlg.exec() == QDialog.Accepted:
             return
 
-        # Create an IC4 EmbeddedDisplay that is using video_widget from above as presentation area
-        display = ic4.EmbeddedDisplay(_window_handle(video_widget))
-        # Configure the display to neatly stretch and center the live image
+        # Create a display widget that will show live video
+        display_widget = ic4.pyside6.DisplayWidget()
+        self.setCentralWidget(display_widget)
+
+        # Get ic4 display object from display widget and configure rendering options
+        display = display_widget.as_display()
         display.set_render_position(ic4.DisplayRenderPosition.STRETCH_CENTER)
 
         try:
+            # Reset all camera settings to default
             self.grabber.device_property_map.set_value(ic4.PropId.USER_SET_SELECTOR, "Default")
             self.grabber.device_property_map.execute_command(ic4.PropId.USER_SET_LOAD)
         except ic4.IC4Exception:
@@ -45,16 +41,15 @@ class MainWindow(QMainWindow):
 
 
 if __name__ == "__main__":
-    # This is just the default Qt main function template, with added calls to Library.init() and Library.exit()
+    # This is just the default Qt main function template, with added call to Library.init_context()
     from sys import argv
 
     app = QApplication(argv)
+    app.setStyle("fusion")
 
-    ic4.Library.init()
+    with ic4.Library.init_context():
 
-    wnd = MainWindow()
-    wnd.show()
+        wnd = MainWindow()
+        wnd.show()
 
-    app.exec()
-
-    ic4.Library.exit()
+        app.exec()
