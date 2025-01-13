1272e39e7e504dff499f84859b331abe0b162deb
Christopher Koschack <christopher.koschack@theimagingsource.com>
2025-01-13 07:33:14 +0100

    Maybe fix GCC9 build error in ic4-demoapp

--- cpp/qt6/demoapp/mainwindow.cpp
+++ cpp/qt6/demoapp/mainwindow.cpp
@@ -265,7 +265,7 @@ void MainWindow::createUI()
 			std::error_code ec;
 			std::filesystem::remove( _devicefile, ec );
 			if (ec) {
-				qWarning().noquote() << "Failed to delete: " << QString(_devicefile.c_str());
+				qWarning().noquote() << "Failed to delete: " << QString::fromStdString(_devicefile.string());
 			}
 		}
 	);
