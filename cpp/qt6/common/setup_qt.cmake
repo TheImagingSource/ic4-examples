52ae34fe459e88cf9b0b5ef3f2843a498c30391e
Christopher Koschack <christopher.koschack@theimagingsource.com>
2025-03-28 10:16:20 +0100

    Fix arm64 qt path lookup

--- cpp/qt6/common/setup_qt.cmake
+++ cpp/qt6/common/setup_qt.cmake
@@ -2,19 +2,29 @@
 if((NOT Qt6_FOUND) AND (NOT Qt5_FOUND) )
 
     if(WIN32)
-        # This path probably will not be valid on your system.
-        # If cmake complains about not being able to find Qt, point it into your Qt installation directory.
-        list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.9.0/msvc2022_64/")	# currently in beta - added 2025/03/06
-        list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.8.2/msvc2022_64/")	# 6.8.3 - added 2025/03/06
-        list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.8.1/msvc2022_64/")	# 6.8.1 - MSVC 2022 (only has msvc2022) added 2024/12/02
-        list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.7.3/msvc2022_64/")	# 6.7.3 - MSVC 2022
-        list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.7.3/msvc2019_64/")	# 6.7.3 - MSVC 2019
-        list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.7.2/msvc2019_64/")	# 
-        list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.7.1/msvc2019_64/")
-        list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.6.3/msvc2019_64/")
-        list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.6.2/msvc2019_64/")
-        list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.6.1/msvc2019_64/")
-        list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.6.0/msvc2019_64/")
+        if( "${IC4_PACAKGE_ARCH}" STREQUAL "arm64" )
+            list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.9.0/msvc2022_arm64/")	# currently in beta - added 2025/03/06
+        	list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.8.3/msvc2022_arm64/")	# 
+        	list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.8.2/msvc2022_arm64/")	# 
+            list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.8.1/msvc2022_arm64/")	# 6.8.1 - MSVC 2022 (only has msvc2022) added 2024/12/02
+            list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.7.3/msvc2022_arm64/")	# 6.7.3 - MSVC 2022
+        else()
+            # This path probably will not be valid on your system.
+            # If cmake complains about not being able to find Qt, point it into your Qt installation directory.
+            list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.9.0/msvc2022_64/")	# currently in beta - added 2025/03/06
+            list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.8.3/msvc2022_64/")	# 6.8.3 - added 2025/03/28
+            list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.8.2/msvc2022_64/")	# 6.8.2 - added 2025/03/06
+            list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.8.1/msvc2022_64/")	# 6.8.1 - MSVC 2022 (only has msvc2022) added 2024/12/02
+            list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.7.3/msvc2022_64/")	# 6.7.3 - MSVC 2022
+            list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.7.3/msvc2019_64/")	# 6.7.3 - MSVC 2019
+            list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.7.2/msvc2019_64/")	# 
+            list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.7.1/msvc2019_64/")
+            list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.6.3/msvc2019_64/")
+            list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.6.2/msvc2019_64/")
+            list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.6.1/msvc2019_64/")
+            list(APPEND CMAKE_PREFIX_PATH "C:/Qt/6.6.0/msvc2019_64/")
+        endif()
+
     endif()
 
     # ubuntu20 only offer qt5 check first for Qt6 and then fall back to Qt5
@@ -33,6 +43,12 @@ if((NOT Qt6_FOUND) AND (NOT Qt5_FOUND) )
             set( QT_VERSION ${Qt6Core_VERSION} )
             set( QT_VERSION_MAJOR 6 )               # This is only needed for Qt5, but to keep it consistent we define it for both
         endif()
+
+        if( "${IC4_PACAKGE_ARCH}" STREQUAL "arm64" )
+            set(IC4_WINDEPLOYQT_EXE  "${QT6_INSTALL_PREFIX}/../msvc2022_64/bin/windeployqt.exe")
+	        set(IC4_WINDEPLOYQT_PATHS "${QT6_INSTALL_PREFIX}/bin/qtpaths.bat")
+        endif()
+
     else()
         find_package(Qt5 QUIET COMPONENTS Core)
         if( Qt5_FOUND )
