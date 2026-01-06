a733df15b7b174b09d463c98eb0929bf3d5eea19
Tim Cassens <tim.cassens@theimagingsource.com>
2026-01-06 11:15:22 +0100

    Enhance Python OpenCV Live example
    The example now shows how to display temporary image buffers that were allocated from a buffer pool.

--- python/thirdparty-integration/imagebuffer-numpy-opencv-live/imagebuffer-numpy-opencv-live.py
+++ python/thirdparty-integration/imagebuffer-numpy-opencv-live/imagebuffer-numpy-opencv-live.py
@@ -5,7 +5,7 @@ from PySide6.QtWidgets import QApplication
 
 
 class ProcessAndDisplayListener(ic4.QueueSinkListener):
-    # Listener to demonstrate processing and displaying received images
+    # Listener to demonstrate in-place-processing and displaying received images
 
     def __init__(self, d: ic4.Display):
         self.display = d
@@ -23,7 +23,7 @@ class ProcessAndDisplayListener(ic4.QueueSinkListener):
         # which is guaranteed by them both not being passed out of this function
         buffer_wrap = buffer.numpy_wrap()
 
-        # Blur the buffer in-place using a rather large kernel
+        # Blur the buffer in place using a rather large kernel
         cv2.blur(buffer_wrap, (31, 31), buffer_wrap)
 
         # Write some text so that the user doesn't hopelessly try to focus the lens
@@ -41,6 +41,58 @@ class ProcessAndDisplayListener(ic4.QueueSinkListener):
         self.display.display_buffer(buffer)
 
 
+class ProcessAndDisplayNewBufferListener(ic4.QueueSinkListener):
+    # Listener to demonstrate processing and displaying images that are transformed into a new image buffer
+
+    def __init__(self, d: ic4.Display):
+        self.display = d
+
+        # Create a buffer pool to take image buffers from
+        # Since one of the buffers is always displayed and the other is used in processing,
+        # let the BufferPool cache two of them to avoid repeated allocations
+        self.buffer_pool = ic4.BufferPool(cache_buffers_max=2)
+
+    def sink_connected(self, sink: ic4.QueueSink, image_type: ic4.ImageType, min_buffers_required: int) -> bool:
+        # Just accept whatever is passed
+        return True
+
+    def frames_queued(self, sink: ic4.QueueSink):
+        # Get the new buffer from the sink
+        src_buffer = sink.pop_output_buffer()
+
+        # Create a numpy view onto the source buffer
+        # This view is only valid while the buffer itself exists,
+        # which is guaranteed by them both not being passed out of this function
+        src_buffer_wrap = src_buffer.numpy_wrap()
+
+        # Get a buffer from the buffer pool with width and height swapped so that we can rotate into it
+        src_type = src_buffer.image_type
+        dest_type = src_buffer.image_type.with_size(new_width=src_type.height, new_height=src_type.width)
+        dest_buffer = self.buffer_pool.get_buffer(dest_type)
+
+        # Create a numpy view onto the destination buffer
+        # This view is only valid while the buffer itself exists,
+        # which is guaranteed by them both not being passed out of this function
+        dest_buffer_wrap = dest_buffer.numpy_wrap()
+
+        # Rotate by 90 degrees from the source into destination buffer
+        cv2.rotate(src_buffer_wrap, cv2.ROTATE_90_CLOCKWISE, dest_buffer_wrap)
+
+        # Write some text into the modified buffer
+        cv2.putText(
+            dest_buffer_wrap,
+            "This image is rotated using OpenCV",
+            (100, 100),
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=1,
+            color=(255, 0, 0),
+            thickness=2,
+        )
+
+        # Display the rotated buffer
+        self.display.display_buffer(dest_buffer)
+
+
 def example_imagebuffer_numpy_opencv_live():
 
     # Create PySide6 application object
@@ -48,13 +100,21 @@ def example_imagebuffer_numpy_opencv_live():
     app.setApplicationDisplayName("IC4 ImageBuffer to NumPy/OpenCV with Live Display")
     app.setStyle("fusion")
 
-    # Let the select a video capture device
+    # Let the user select a video capture device
     device_list = ic4.DeviceEnum.devices()
     for i, dev in enumerate(device_list):
         print(f"[{i}] {dev.model_name} ({dev.serial}) [{dev.interface.display_name}]")
     print(f"Select device [0..{len(device_list) - 1}]: ", end="")
     selected_index = int(input())
     dev_info = device_list[selected_index]
+
+    # Let the user choose whether we want to rotate
+    print()
+    print("Available image processing operations:")
+    print("[0] Blur buffer in place")
+    print("[1] Rotate into new buffer")
+    print("Select operation [0..1] ", end="")
+    operation = int(input())
 
     # Open the selected device in a new Grabber
     grabber = ic4.Grabber()
@@ -69,7 +129,7 @@ def example_imagebuffer_numpy_opencv_live():
     display.set_render_position(ic4.DisplayRenderPosition.STRETCH_CENTER)
 
     # Create a listener to process and display the received images
-    listener = ProcessAndDisplayListener(display)
+    listener = ProcessAndDisplayListener(display) if operation == 0 else ProcessAndDisplayNewBufferListener(display)
 
     # Create a sink that passes the images to the listener
     # This sink fixes the pixel format to BGR8, so that the listener receives 3-channel color images
